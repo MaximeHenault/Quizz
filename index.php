59cb1534901329eb60764defb58206e37f1ee91e
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-04-04 14:02:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include('appmvc.php');
+
+	if(isset($_GET['page'])) $page = $_GET['page'];
+	else $page = 1;
+
+	if(isset($_GET['question'])) $question = $_GET['question'];
+    else $question = 1; 
+
+	if(isset($_GET['compt'])) $compt = $_GET['compt'];
+    else $compt = 0; 
+
+	$monapp = new AppMVC();
+	
+	$monapp -> afficherPage($page, $question, $compt);
+?>
